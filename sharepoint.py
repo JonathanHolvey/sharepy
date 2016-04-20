1462103e26f4acca5d90f9c7c801070dd29101b1
Jonathan Holvey <jonathan.holvey@lumascape.com>
2016-04-21 09:01:52 +1000

    Add error handling and messages

--- sharepoint.py
+++ sharepoint.py
@@ -22,12 +22,18 @@ class SPSession:
 		samlRequest = samlRequest.replace("[endpoint]", self.site)
 
 		# request STS token from Microsoft Online
+		print("Requesting STS token...")
 		response = requests.post("https://login.microsoftonline.com/extSTS.srf", data = samlRequest)
 		# parse and extract token from returned XML
-		root = et.fromstring(response.text)
-		token = root.find(".//{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}BinarySecurityToken").text
+		try:
+			root = et.fromstring(response.text)
+			token = root.find(".//{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}BinarySecurityToken").text
+		except:
+			print("Token request failed. Check your username and password\n")
+			return
 
 		# request authorisation from sharepoint site
+		print("Requesting authorisation cookies...")
 		response = requests.post("https://" + self.site + "/_forms/default.aspx?wa=wsignin1.0", data = token, headers = {"Host": self.site})
 
 		# parse authorisation cookies from returned headers
@@ -39,6 +45,9 @@ class SPSession:
 			if match[0:5] == "rtFa=" or match[0:8] == "FedAuth=":
 				cookies.append(match)
 
+		if len(cookies) != 2:
+			print("Cookie request failed. Check your SharePoint site URL\n")
+
 		cookie = "; ".join(cookies)
 
 		# verify authorisation by requesting page
@@ -47,10 +56,10 @@ class SPSession:
 		if response.status_code == requests.codes.ok:
 			self.cookie = cookie
 			self.expire = 0
-			print("Authentication successful")
+			print("Authentication successful\n")
 		else:
 			self.expire = None
-			print("Authentication failed")
+			print("Authentication failed\n")
 
 	def get(self, requestURI):
 		return requests.get(requestURI, headers = {"Cookie": self.cookie, "Accept": "application/json; odata=verbose"})
