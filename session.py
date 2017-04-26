d6ebefeed2ba71409afc2a7a7c8ca89503da39f2
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-04-27 08:26:22 +1000

    Fix miss-matched quotes in SharePointSession docstring

--- session.py
+++ session.py
@@ -40,7 +40,7 @@ class SharePointSession(requests.Session):
     Basic Usage::
       >>> import sharepy
       >>> s = sharepy.connect("example.sharepoint.com")
-      >>> s.get('https://exemple.sharepoint.com/_api/web/lists')
+      >>> s.get("https://exemple.sharepoint.com/_api/web/lists")
       <Response [200]>
     """
 
