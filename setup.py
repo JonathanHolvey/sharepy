796c6a8f2f0033f8de23ebc68d9e7cac1e65eaf2
tschroedertlc <tschroeder@thelarkincompany.com>
2019-05-23 09:18:36 -0700

    Added setup.py in order to install from Github

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='sharepy',
+      version='1.3.0',
+      description='Simple SharePoint Online authentication for Python',
+      url='https://github.com/tschroedertlc/sharepy',
+      author='Jonathon Holvey',
+      author_email='jonathan.holvey@outlook.com',
+      license='GPLv3',
+      packages=['shareoy'],
+      zip_safe=False)
