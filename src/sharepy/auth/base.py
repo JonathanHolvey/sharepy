022a1878d04340d14b22a0ab57d741e0f65b5da3
Jonathan Holvey <jonathan.holvey@outlook.com>
2020-01-18 10:08:25 +1000

    Refactor SharePointOnline auth to use base auth class

--- src/sharepy/auth/base.py
+++ src/sharepy/auth/base.py
@@ -1,44 +1,18 @@
-from datetime import datetime
-import xml.etree.ElementTree as et
-
-import requests
+from requests.auth import AuthBase as RequestsAuth
 
 
-class BaseAuth(requests.auth.AuthBase):
+class BaseAuth(RequestsAuth):
+    site = None
+
     """A base interface that all SharePy auth classes should inherit from"""
-    def __init__(self, username, password=None, auth_url=None):
-        self.site = None
+    def __init__(self, username, password=None):
         self.username = username
         self.password = password
-        self.auth_url = auth_url or self.auth_url
-        self.expire = datetime.now()
 
     def login(self, site):
         """Perform authentication steps"""
-        raise NotImplementedError("Please implement this method")
+        raise NotImplementedError("Auth classes must allow login")
 
     def refresh(self):
-        """Refresh any expiring"""
-        raise NotImplementedError("Please implement this method")
-
-
-class SoapAuth(BaseAuth):
-    xml = et
-    ns = {
-        "d": "http://schemas.microsoft.com/ado/2007/08/dataservices",
-        "ds": "http://www.w3.org/2000/09/xmldsig#",
-        "ec": "http://www.w3.org/2001/10/xml-exc-c14n#",
-        "psf": "http://schemas.microsoft.com/Passport/SoapServices/SOAPFault",
-        "S": "http://www.w3.org/2003/05/soap-envelope",
-        "saml": "urn:oasis:names:tc:SAML:1.0:assertion",
-        "soap": "http://schemas.microsoft.com/sharepoint/soap/",
-        "wsse": "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd",
-        "xs": "http://www.w3.org/2001/XMLSchema",
-        "xsi": "http://www.w3.org/2001/XMLSchema-instance",
-    }
-
-    def __init__(self, *args, **kwargs):
-        super()
-        # Register XML namespaces
-        for alias, uri in self.ns.items():
-            self.xml.register_namespace(alias, uri)
+        """Refresh any expiring tokens or cookies"""
+        raise NotImplementedError("Auth classes must allow refresh")
---
+++ src/sharepy/auth/spol.py
@@ -0,0 +1,106 @@
+import os
+from getpass import getpass
+from datetime import datetime, timedelta
+from xml.sax.saxutils import escape
+
+import requests
+
+from .base import BaseAuth
+from .xml import ns, ElementTree as et
+from .. import errors
+
+
+class SharePointOnline(BaseAuth):
+    auth_domain = "login.microsoftonline.com"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.expire = datetime.now()
+        self.token = None
+        self.cookie = None
+        self.digest = None
+
+    def login(self, site):
+        """Perform authentication steps"""
+        self.site = site
+        self._get_token()
+        self._get_cookie()
+        self._get_digest()
+
+    def refresh(self):
+        self._get_digest()
+
+    def __call__(self, request):
+        """Inject cookies into requests as they are made"""
+        if self.cookie and self.digest:
+            request.headers.update({"Content-Type": "text/xml; charset=utf-8",
+                                    "Cookie": self.cookie})
+        return request
+
+    def _get_token(self):
+        """Request authentication token from Microsoft"""
+        # Load SAML request template
+        with open(os.path.join(os.path.dirname(__file__),
+                               "templates/spol-token.saml"), "r") as file:
+            saml = file.read()
+
+        # Insert username and password into SAML request after escaping special characters
+        password = self.password or getpass("Enter your password: ")
+        saml = saml.format(username=escape(self.username),
+                           password=escape(password),
+                           site=self.site)
+
+        # Request security token from Microsoft Online
+        auth_url = "https://{}/extSTS.srf".format(self.auth_domain)
+        response = requests.post(auth_url, data=saml)
+        # Parse and extract token from returned XML
+        try:
+            root = et.fromstring(response.text)
+        except et.ParseError:
+            raise errors.AuthError("Token request failed. Invalid server response")
+
+        # Extract token from returned XML
+        token = root.find(".//wsse:BinarySecurityToken", ns)
+        # Check for errors and print error messages
+        if token is None or root.find(".//S:Fault", ns) is not None:
+            raise errors.AuthError("{}: {}".format(root.find(".//S:Text").text,
+                                   root.find(".//psf:text").text).strip().strip("."))
+        self.token = token.text
+
+    def _get_cookie(self):
+        """Request access cookie from sharepoint site"""
+        response = requests.post("https://" + self.site + "/_forms/default.aspx?wa=wsignin1.0",
+                                 data=self.token, headers={"Host": self.site})
+
+        # Create access cookie from returned headers
+        cookie = self._buildcookie(response.cookies)
+        # Verify access by requesting page
+        response = requests.get("https://" + self.site + "/_api/web", headers={"Cookie": cookie})
+
+        if response.status_code == requests.codes.ok:
+            self.cookie = cookie
+        else:
+            raise errors.AuthError("Authentication failed")
+
+    def _get_digest(self):
+        """Check and refresh sites cookie and request digest"""
+        if self.expire <= datetime.now():
+            # Request site context info from SharePoint site
+            response = requests.post("https://" + self.site + "/_api/contextinfo",
+                                     data="", headers={"Cookie": self.cookie})
+            # Parse digest text and timeout from XML
+            try:
+                root = et.fromstring(response.text)
+                self.digest = root.find(".//d:FormDigestValue", ns).text
+                self.cookie = self._buildcookie(response.cookies)
+                timeout = int(root.find(".//d:FormDigestTimeoutSeconds", ns).text)
+            except Exception:
+                raise errors.AuthError("Digest request failed")
+            # Calculate digest expiry time
+            self.expire = datetime.now() + timedelta(seconds=timeout)
+
+        return True
+
+    def _buildcookie(self, cookies):
+        """Create session cookie from response cookie dictionary"""
+        return "rtFa=" + cookies["rtFa"] + "; FedAuth=" + cookies["FedAuth"]
